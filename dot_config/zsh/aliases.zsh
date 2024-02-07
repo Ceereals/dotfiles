c361db02efe15d3010118b6cc54d9aa5a492988d
Riccardo Romoli <riccardo.romoli@u2y.io>
2024-02-07 08:57:33 +0100

    First commit

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,51 @@
+# Alias
+# ---
+#
+# alias k="kubectl"
+# alias h="helm"
+# alias tf="terraform"
+# alias a="ansible"
+# alias ap="ansible-playbook"
+# alias dt="datree"
+
+# mac OS shortcuts
+# alias code="open -a 'Visual Studio Code'"
+
+
+
+# ALIAS COMMANDS
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons --group-directories-first -l"
+alias g="goto"
+alias grep='grep --color'
+alias update="source ~/.zshrc"
+alias change="code ~/.zshrc"
+alias pub="flutter pub"
+alias gam="git commit -am"
+alias gp="git push"
+alias prepare="~/.dotfiles/init.sh"
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+alias pest='./vendor/bin/pest'
+alias run='docker-compose run --rm'
+
+# PHP
+
+installedPhpVersions=($(brew ls --versions | ggrep -E 'php(@.*)?\s' | ggrep -oP '(?<=\s)\d\.\d' | uniq | sort))
+
+for phpVersion in ${installedPhpVersions[*]}; do
+    value="{"
+
+    for otherPhpVersion in ${installedPhpVersions[*]}; do
+        if [ "${otherPhpVersion}" = "${phpVersion}" ]; then
+            continue
+        fi
+
+        # unlink other PHP version
+        value="${value} brew unlink php@${otherPhpVersion};"
+    done
+
+    # link desired PHP version
+    value="${value} brew link php@${phpVersion} --force --overwrite; } &> /dev/null && php -v"
+
+    alias "${phpVersion}"="${value}"
+done
---
+++ dot_config/zsh/nvm.zsh
@@ -0,0 +1,28 @@
+# NVM lazy load
+if [ -s "$HOME/.nvm/nvm.sh" ]; then
+  [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+  alias nvm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && nvm'
+  alias node='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && node'
+  alias npm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && npm'
+fi
+
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc 
