b3cbfd770cc10d9387a34577b8bc2abe63e9a1ed
Ceereals <romoliriccardo.rr@gmail.com>
2026-05-03 23:37:08 +0200

    Add .config/fish/config.fish Add .config/fish/fish_variables

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,10 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+zoxide init fish | source
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# opencode
+fish_add_path /home/ceereals/.opencode/bin
