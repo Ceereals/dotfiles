690a499662847275d0ce921820378d6578ad66c1
Ceereals <romoliriccardo.rr@gmail.com>
2026-05-04 00:50:46 +0200

    Update .config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 zoxide init fish | source
 starship init fish | source
 mise activate fish | source
+zoxide init --cmd cd fish | source
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
