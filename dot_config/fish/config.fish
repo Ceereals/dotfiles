bfdc5265075043f1dc730677b3efcae90cb97a42
Ceereals <romoliriccardo.rr@gmail.com>
2026-05-04 18:00:54 +0200

    Update .config/btop/btop.conf Update .config/fish/config.fish Update .config/niri/cfg/display.kdl Update .config/noctalia/settings.json

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,6 +3,7 @@ zoxide init fish | source
 starship init fish | source
 mise activate fish | source
 zoxide init --cmd cd fish | source
+direnv hook fish | source
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
